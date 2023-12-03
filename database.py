5c0711b4d0ca7f3842be7864bf164cd1cdb05ea0
SaaighTaimur <146673423+SaaighTaimur@users.noreply.github.com>
2023-12-03 11:01:07 -0800

    Update database.py

--- database.py
+++ database.py
@@ -28,3 +28,16 @@ def insert_period(slider_rating):
     # Insert the data with the unique identifier as the key
     return db.put(data)
 
+def get_average_rating():
+    # Retrieve all ratings from the database
+    all_ratings = list(db.fetch().items)
+    
+    if not all_ratings:
+        return None
+    
+    # Calculate the average rating
+    total_ratings = len(all_ratings)
+    total_sum = sum(item["rating"] for item in all_ratings)
+    average_rating = total_sum / total_ratings
+    
+    return average_rating
